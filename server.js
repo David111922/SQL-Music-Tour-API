489dbe6080d44863fca1a7950fc8e18ef74318c3
David111922 <salve62@yahoo.com>
2024-07-10 21:37:36 -0700

    controller causing issues

--- server.js
+++ server.js
@@ -35,6 +35,10 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+// CONTROLLERS 
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
